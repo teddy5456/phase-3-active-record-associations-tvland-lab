6edfecf4162fcbbb68a970a5326e47f2eaa9dcac
teddy <teddywambua5456@gmail.com>
2023-06-05 01:00:15 +0300

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
   
-end
+    def actors_list
+      actors.map(&:full_name)
+    end
+  end
